d0d4bcbdb38a1f9aca50baf602df9d092802464c
Poelsk <camiloperezdonoso@gmail.com>
2025-07-19 11:59:04 -0400

    version expv2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.nationtech"
-version = "0.1.1-BETA"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -15,12 +15,11 @@ repositories {
     maven("https://oss.sonatype.org/content/groups/public/")
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.dmulloy2.net/repository/public/")
-    maven("https://jitpack.io") // <<-- REAÑADIDO: Repositorio de JitPack
+    maven("https://jitpack.io")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
-    // <<-- CAMBIO CLAVE: Volviendo a la dependencia de Towny a través de JitPack
     compileOnly("com.github.TownyAdvanced:Towny:0.101.1.10")
 }
 
